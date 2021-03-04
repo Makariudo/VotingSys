63621b613e5e3d2acc0483f8c35edafd5877f887
Makariudo <sspassevitch@gmail.com>
2021-03-04 23:25:28 +0100

    test V1

--- test/Voting.js
+++ test/Voting.js
@@ -1,25 +1,27 @@
-
+const truffleAssert = require('truffle-assertions');
 const Voting = artifacts.require("./Voting.sol");
-/*
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
-
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
-
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
-
-    assert.equal(storedData, 89, "The value 89 was not stored.");
-  });
-});*/
 
 contract("Voting", accounts => {
-  it("should deploy", async () => {
-    const votingInstance = await Voting.deployed();
-    //const balance = votingInstance.getBalance.call(accounts[0])
-    assert(votingInstance.address)
+  let [admin,account1,account2] = accounts;
+  let contractInstance;
+  let proposalTesting = "Sergio";
+
+  beforeEach(async () => {
+      contractInstance = await Voting.new();
   });
+
+  it("should deploy", async () => {
+    assert(contractInstance.address)
+  });
+
+  it("should return accounts[0] is owner", async () => {
+    let owner = await contractInstance.owner();
+    assert.equal(owner, admin);
+  })
+
+  it("should fail to add voter", async () => {
+    await truffleAssert.reverts(contractInstance.addVoter(account1, {
+      'from': accounts[1]}))
 });
 
+})
