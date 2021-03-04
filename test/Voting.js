8ddb2028d6a382bee2833edfe00f97397eebf755
Sandy Instructor <sandyludosky@gmail.com>
2021-03-04 17:15:18 +0100

    add test/Voting.js

--- test/Voting.js
+++ test/Voting.js
@@ -1,25 +1,23 @@
-
-const Voting = artifacts.require("./Voting.sol");
-/*
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
-
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
-
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
-
-    assert.equal(storedData, 89, "The value 89 was not stored.");
-  });
-});*/
+const { assert } = require("chai");
+const Voting = artifacts.require("Voting");
 
 contract("Voting", accounts => {
+  let [account1, account2] = accounts;
+  let contractInstance;
+  beforeEach(async () => {
+    contractInstance = await Voting.new();
+  });
+
   it("should deploy", async () => {
     const votingInstance = await Voting.deployed();
     //const balance = votingInstance.getBalance.call(accounts[0])
     assert(votingInstance.address)
   });
-});
 
+  it("workflow status should be RegisteringVoters", () => {
+    return Voting.deployed()
+      .then(instance => instance.status())
+      .then(status => assert.equal(status.toNumber(), 0), "status should be RegisteringVoters upon deploy")
+  })
+  it("")
+})
